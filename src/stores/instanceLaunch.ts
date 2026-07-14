2455c029356f7de4646139896d648061f1789f85
happygray <Ds_140301@hotmail.com>
2026-07-14 13:14:02 +0800

    删除

--- src/stores/instanceLaunch.ts
+++
@@ -1,5 +0,0 @@
-import { ref } from 'vue'
-
-export const currentInstanceName = ref<string | null>(null)
-export const currentLaunchFn = ref<(() => Promise<void>) | null>(null)
-export const currentStopFn = ref<(() => Promise<void>) | null>(null)
